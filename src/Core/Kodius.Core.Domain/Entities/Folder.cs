aad34324b0d4b52679f245b9b4209937b2d9b13b
Saman Qaydi (Desktop) <saman.qaydi@live.com>
2022-07-01 10:23:33 +0200

    Add Domain Project

---
+++ src/Core/Kodius.Core.Domain/Entities/Folder.cs
@@ -0,0 +1,7 @@
+﻿namespace Kodius.Core.Domain.Entities;
+
+public class Folder
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+}
---
+++ src/Core/Kodius.Core.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Kodius.Core.Domain.Entities;
+
+public class User
+{
+    public long Id { get; set; }
+    public string Email { get; set; }
+    public byte[] HashPassword { get; set; }
+    public byte[] HashSalt { get; set; }
+}
